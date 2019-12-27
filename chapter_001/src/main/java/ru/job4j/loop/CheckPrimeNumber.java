1653569ea01c5f6f23a130c62d73c75ba74decca
Vitaly Makarov <vbmakarov.info@gmail.com>
2019-12-28 00:54:40 +0300

    Простые числа.[CheckPrimeNumber с циклом без 2]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,9 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean isPrimeNumber=true;
-        if (number == 2) {
-            return isPrimeNumber;
-        }
         for (int i = 2; i < number; i++) {
-            if (number % i == 0) isPrimeNumber = false;
+            if (number % i == 0) return false;
         }
-        return isPrimeNumber;
+        return true;
     }
 }
