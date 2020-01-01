c56832e1abab32b5d3f3ff784f29e321ec1f5e96
Vitaly Makarov <vbmakarov.info@gmail.com>
2020-01-01 22:28:04 +0300

    Ипотека.[#160088]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+
+        // вычисляем сумму которую нам нужно выплатить с учеттом проценной ставки
+        double credit=amount+amount*(percent/100);
+
+        //остаток по кредиту с учетом процентной ставки
+        double balance;
+
+        do{
+            balance=credit-salary;
+            credit=balance*(percent/100)+balance;
+            year++;
+        }
+        while(credit>0);
+        return year;
+    }
+}
