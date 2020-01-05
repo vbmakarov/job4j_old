57487a934681fd6c046cc81a16a06999e8539652
Vitaly Makarov <vbmakarov.info@gmail.com>
2020-01-05 16:08:11 +0300

    Перевернуть массив.[Версия №2]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Turn {
     public int[] back(int[] array) {
-        for (int i=1;i<array.length-1;i++) {
+        for (int i=1;i<array.length/2+1;i++) {
             int tmp=array[array.length-i];
             array[array.length-i]=array[i-1];
             array[i-1]=tmp;
