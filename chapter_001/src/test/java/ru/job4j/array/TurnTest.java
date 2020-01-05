5f7d2f92ed14ff07b5d159df63295d748dde0321
Vitaly Makarov <vbmakarov.info@gmail.com>
2020-01-05 16:08:32 +0300

    Перевернуть массив.[Тест №2]

--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TurnTest {
+
     @Test
     public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
         Turn turner = new Turn();
@@ -21,6 +22,33 @@ public class TurnTest {
         int[] input = new int[] {1, 2, 3, 4, 5};
         int[] result = turner.back(input);
         int[] expect = new int[] {5,4,3,2,1};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void test() {
+        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+        Turn turner = new Turn();
+        int[] input = new int[] {1, 2, 3, 4, 5,6,7};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {7,6,5,4,3,2,1};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void test2() {
+        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+        Turn turner = new Turn();
+        int[] input = new int[] {1, 2, 3, 4, 5,6,7,8,9};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {9,8,7,6,5,4,3,2,1};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void test3() {
+        Turn turner = new Turn();
+        int[] input = new int[] {4, 1};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {1, 4};
         assertThat(result, is(expect));
     }
 }
