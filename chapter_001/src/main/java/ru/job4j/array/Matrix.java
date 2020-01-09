2cf141fae01e5bc6b516a99d51de368d82aa7b2a
Vitaly Makarov <vbmakarov.info@gmail.com>
2020-01-09 12:29:16 +0300

    Квадратный массив заполнен true или false по диагоналям.[#160095]

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -10,4 +10,57 @@ public class Matrix {
         }
         return table;
     }
+
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        int indexH=0, indexG=0;
+        boolean init=false;
+        for (int i=0;i<board.length;i++) {
+            for(int j=0;j<board.length;j++) {
+                if (board[i][j]=='X') {
+                    indexH=j;
+                    indexG=i;
+                    init=true;
+                    break;
+                }
+            }
+            if(init){
+                break;
+            }
+        }
+        if(monoHorizontal(board, indexG) || monoVertical(board, indexH)){
+            result=true;
+        }
+
+        return result;
+    }
+    public static boolean monoVertical(char[][] board, int index){
+        boolean result=false;
+        for(int i=0;i<board.length;i++){
+            if(board[i][index]=='X'){
+                result=true;
+            }
+            else{
+                result=false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoHorizontal(char[][] board, int index){
+        boolean result=false;
+        for(int i=0;i<board.length;i++){
+            if(board[index][i]=='X'){
+                result=true;
+            }
+            else{
+                result=false;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
