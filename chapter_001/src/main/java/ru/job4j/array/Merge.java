5ea4f9f97718912202471ed1432614452f528cfe
Vitaly Makarov <vbmakarov.info@gmail.com>
2020-01-25 12:12:32 +0300

    Объединить два массива [поле проверки по Checkstyle]

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -8,25 +8,25 @@ public class Merge {
         int[] rsl = new int[left.length + right.length];
         int onePt = 0;
         int twoPt = 0;
-            while(onePt+twoPt<rsl.length){
-                if(twoPt==right.length){
-                    rsl[onePt+twoPt]=left[onePt];
+            while (onePt + twoPt < rsl.length) {
+                if (twoPt == right.length) {
+                    rsl[onePt + twoPt] = left[onePt];
                     onePt++;
-                }else if(onePt==left.length){
-                    rsl[onePt+twoPt]=right[twoPt];
+                } else if (onePt == left.length) {
+                    rsl[onePt + twoPt] = right[twoPt];
                     twoPt++;
-                } else if(left[onePt]<=right[twoPt]){
-                    rsl[onePt+twoPt]=left[onePt];
+                } else if (left[onePt] <= right[twoPt]) {
+                    rsl[onePt + twoPt] = left[onePt];
                     onePt++;
-                }else if(left[onePt]>right[twoPt]){
-                    rsl[onePt+twoPt]=right[twoPt];
+                } else if (left[onePt] > right[twoPt]) {
+                    rsl[onePt + twoPt] = right[twoPt];
                     twoPt++;
                 }
         }
         return rsl;
     }
 
-        public static void main (String[]args) {
+        public static void main(String[] args) {
             Merge process = new Merge();
             int[] rsl = process.merge(
                     new int[]{1, 3, 5},
